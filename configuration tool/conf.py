6c3eb1fe5c00005fecfdccce6920d5d5613b5191
Ernest Carozza <caroze@rpi.edu>
2010-04-25 23:25:36 -0400

    Merged's Sean changes with mine from April 21. Adds support for Open Project window (to be expanded upon slightly)

--- configuration tool/conf.py
+++ configuration tool/conf.py
@@ -17,12 +17,16 @@ DET_TYPE_DENY = 0
 DET_TYPE_TYPE = 1
 DET_TYPE_ROLES = 2
 
+PROJECT_DIR="./proj/"
+PROJECT_EXT=".cf"
 
 mySites=[]
-rbRadios=[]
+#rbRadios=[]
 picRole=0
 lastselection = None
 myRolesList=[]
+iProjType=0
+
 
 ROLE_FILE_NAME="myRoles"
 #roleFile=open(ROLE_FILE_NAME,'w')
@@ -72,7 +76,7 @@ def addSite():
 
 def openFile():
 	global mySites
-        infile = tkFileDialog.askopenfile(parent=root,mode='rb',title='Choose a configuration file to open')
+        infile = tkFileDialog.askopenfile(parent=setting,mode='rb',title='Choose a configuration file to open')
 	if infile != None:
 		mySites = pickle.load(infile)
 		if mySites == None or len(mySites) == 0:
@@ -83,11 +87,12 @@ def openFile():
 			lbSiteList.insert(END, k['url'])
 
 def saveFile():
-	fileName = tkFileDialog.asksaveasfilename(parent=root, title="Save the configuration file as...")
+	#fileName = tkFileDialog.asksaveasfilename(parent=setting, title="Save the configuration file as...")
+	global eProjName
+	fileName = eProjName.get()
 	if len(fileName) > 1:
-		outfile = open(fileName, 'wb')
+		outfile = open(PROJECT_DIR + '/' + fileName, 'wb')
 		pickle.dump(mySites, outfile)
-		print mySites
 		outfile.close()
 		
 	
@@ -110,6 +115,7 @@ def list_selection_changed(selection):
 	rbTimeRadios[blockmethod].select()
 	if (blockmethod == TIME_TYPE_ALLOW_BREAKS):
 		BreakLengthStr.set(str(configobj['BlockConfig']['BreakLength']))
+		WaitTimeStr.set(str(configobj['BlockConfig']['TimeBetweenBreaks']))
 	elif (blockmethod == TIME_TYPE_BLOCK_SCHEDULING):
 		breaks = configobj['BlockConfig']['AllowedTime']
 		for allowedtime in breaks:
@@ -146,10 +152,18 @@ def lookForEdit(event):
 
 
 def loadRoleModel():
+#<<<<<<< HEAD
+#	global lbRolePrev
+#	fileM=tkFileDialog.askopenfilename(parent=setting,title='Choose a file')
+#	print fileM
+#	if fileM!=None:
+#		myPic=PhotoImage(file=fileM)
+#=======
 	global lbRolePrev, imageFile
-	imageFile=tkFileDialog.askopenfilename(parent=root,title='Choose a file')
+	imageFile=tkFileDialog.askopenfilename(parent=setting,title='Choose a file')
 	if imageFile!=None:
 		myPic=PhotoImage(file=imageFile)
+#>>>>>>> 912814b6d4c9c44d2fffb8f7b4d3e953a44b4b29
 		lbRolePrev.config(image=myPic)
 		lbRolePrev.image = myPic
 		
@@ -162,8 +176,13 @@ def setRoleName():
 	roleName= eRoleName.get()
 	
 def myRoleWindow():
-	global imageFile, tbQuotes, top, lbRolePrev, eRoleName, imageName, eRoleName
-	top=Toplevel(root)
+#<<<<<<< HEAD
+	global imageFile, tbQuotes, top, lbRolePrev, eRoleName, imageName
+	top=Toplevel(setting)
+#=======
+#	global imageFile, tbQuotes, top, lbRolePrev, eRoleName, imageName, eRoleName
+#	top=Toplevel(root)
+#>>>>>>> 912814b6d4c9c44d2fffb8f7b4d3e953a44b4b29
 	
 	lbRoleMo=Label(top,text="Role Model")
 	lbRoleMo.grid(row=0,column=1)
@@ -174,7 +193,7 @@ def myRoleWindow():
 	eRoleName=Entry(fRoleName)
 	eRoleName.grid(row=0,column=1)
 	eRoleName.insert(END,imageName)
-	bRoleButton=Button(fRoleName,text="Set Name",command=setRoleName())
+	bRoleButton=Button(fRoleName,text="Set Name",command=setRoleName)
 	fRoleName.grid(row=1,column=0,sticky=W)
 	
 	lbPreview=Label(top,text="Preview")
@@ -363,20 +382,27 @@ def roleListLoad_old():
 	roleFile.close()
 
 def build_layout():
-	global lbSiteList, SiteStr, intTimeType, BreakLengthStr, WaitTimeStr
-	global liTime, root, rbTimeRadios, intListType, lbRoleModels
+#<<<<<<< HEAD
+#	global lbSiteList, SiteStr, intTimeType, BreakLengthStr
+	global liTime, setting, rbTimeRadios, intListType, lbRoleModels
 	global roleName, roleText, roleFile, myRolesList, tbQuotes, imageFile
-	global lbRolePrev, rbDetRadios, intDetType, entSite, bSite
+#	global lbRolePrev, projects, eProjName
+#=======
+	global lbSiteList, SiteStr, intTimeType, BreakLengthStr, WaitTimeStr
+#	global liTime, root, rbTimeRadios, intListType, lbRoleModels
+	global roleName, roleText, roleFile, myRolesList, tbQuotes, imageFile
+	global lbRolePrev, rbDetRadios, intDetType, entSite, bSite, projects, eProjName, rbRadios
+#>>>>>>> 912814b6d4c9c44d2fffb8f7b4d3e953a44b4b29
 	
-	root= Tk()
-	menubar = Menu(root)
+	setting= Toplevel(projects)
+	menubar = Menu(setting)
 
 	# create a pulldown menu, and add it to the menu bar
 	filemenu = Menu(menubar, tearoff=0)
 	filemenu.add_command(label="Open", command=openFile)
 	filemenu.add_command(label="Save", command=saveFile)
 	filemenu.add_separator()
-	filemenu.add_command(label="Exit", command=root.quit)
+	filemenu.add_command(label="Exit", command=setting.quit)
 	menubar.add_cascade(label="File", menu=filemenu)
 
 	# Placeholder for Help menu
@@ -385,13 +411,14 @@ def build_layout():
 	menubar.add_cascade(label="Help", menu=helpmenu)
 	
 	# display the menu
-	root.config(menu=menubar)
+	setting.config(menu=menubar)
 
 	# create the website list
-	frame = Frame(root)
-	frame.grid(row=1,column=0)
+	fWebsites = Frame(setting)
+	frame = Frame(fWebsites)
+	frame.grid(row=2,column=0)
 
-	laWeb= Label(root, text="Websites")
+	laWeb= Label(fWebsites, text="Websites")
 	laWeb.grid(row=0, column=0)
 
 	SiteStr = StringVar()
@@ -405,29 +432,41 @@ def build_layout():
 	bSite = Button(frame, text="Add Site", command=addSite)
 	bSite.grid(row=0,column=2)
 
-	lbSiteList=Listbox(root,selectmode=EXTENDED, exportselection=0)
-	lbSiteList.grid(row=2, column=0)
+#<<<<<<< HEAD
+	lbSiteList=Listbox(fWebsites,selectmode=EXTENDED, exportselection=0)
+	lbSiteList.grid(row=3, column=0)
+#=======
+#	lbSiteList=Listbox(root,selectmode=EXTENDED, exportselection=0)
+#	lbSiteList.grid(row=2, column=0)
+#>>>>>>> 912814b6d4c9c44d2fffb8f7b4d3e953a44b4b29
 
-	bRemoveSite = Button(root, text="Remove Site", command=removeSite)
-	bRemoveSite.grid(row=3,column=0)
+	bRemoveSite = Button(fWebsites, text="Remove Site", command=removeSite)
+	bRemoveSite.grid(row=4,column=0)
+	
+	
 
 	poll() # Wait for user to click on site in list -- populate right side accordingly
 
 	# Blacklist / Whitelist
 	intListType= IntVar(value=1)
-	fRadios=Frame(root)
+	fRadios=Frame(fWebsites)
+	rbRadios = []
 	rbRadios.append(Radiobutton(fRadios,text="Whitelist", variable=intListType,value=LIST_TYPE_WHITELIST))
 	rbRadios.append(Radiobutton(fRadios,text="Blacklist", variable=intListType,value=LIST_TYPE_BLACKLIST))
 	rbRadios[0].grid(row=0, column=0)
 	rbRadios[1].grid(row=0, column=1)
-	fRadios.grid(row=5,column=0)
+	fRadios.grid(row=6,column=0)
 
+	fWebsites.grid(row=3,column=0)
+	
+	
 	# Blocking Method
-	lTimeSection=Label(root, text="Blocking Method")
-	lTimeSection.grid(row=0, column=4)
+	fBlock= Frame(setting)
+	lTimeSection=Label(fBlock, text="Blocking Method")
+	lTimeSection.grid(row=1, column=4)
 
-	fTimeList=Frame(root)
-	fTimeList.grid(row=2, column=4, padx=15)
+	fTimeList=Frame(fBlock)
+	fTimeList.grid(row=3, column=4, padx=15)
 
 	intTimeType=IntVar()
 	rbTimeRadios = []
@@ -436,7 +475,7 @@ def build_layout():
 	rbTimeRadios.append(Radiobutton(fTimeList,text="Block Scheduling", variable=intTimeType,value=TIME_TYPE_BLOCK_SCHEDULING))
 	rbTimeRadios[0].grid(row=0, column=0, sticky=W)
 	rbTimeRadios[1].grid(row=1, column=0, sticky=W)
-	rbTimeRadios[2].grid(row=3, column=0, sticky=W)
+	rbTimeRadios[2].grid(row=4, column=0, sticky=W)
 
 	# Blocking Method / Allow Breaks
 	fBreakFrame=Frame(fTimeList)
@@ -446,18 +485,24 @@ def build_layout():
 	BreakLengthStr = StringVar()
 	entBreakLength=Entry(fBreakFrame, textvariable=BreakLengthStr)
 	entBreakLength.grid(row=2,column=1)
+#<<<<<<< HEAD
+#	fBreakFrame.grid(row=3,column=0,sticky=E,padx=20)
+#=======
 
 	lbBreakWaitTime = Label(fBreakFrame, text="Time Between Breaks")
 	lbBreakWaitTime.grid(row=3,column=0,sticky=E,padx=2)
 	WaitTimeStr = StringVar()
 	entWaitTime = Entry(fBreakFrame, textvariable=WaitTimeStr)
 	entWaitTime.grid(row=3,column=1)
-	fBreakFrame.grid(row=2,column=0,sticky=E,padx=20)
+#	fBreakFrame.grid(row=2,column=0,sticky=E,padx=20)
+#>>>>>>> 912814b6d4c9c44d2fffb8f7b4d3e953a44b4b29
+
+	fBreakFrame.grid(row=3,column=0,sticky=E,padx=20)
 
 
 	# Blocking Method / Block Scheduling
 	fTimeScroll=Frame(fTimeList)
-	fTimeScroll.grid(row=4,column=0)
+	fTimeScroll.grid(row=6,column=0)
 	scTime = Scrollbar(fTimeScroll, orient=VERTICAL)
 	liTime = Listbox(fTimeScroll, selectmode=EXTENDED,yscrollcommand=scTime.set, exportselection=0)
 	liTime.grid(row=0, column=0, sticky=N+S, rowspan=1)
@@ -471,15 +516,24 @@ def build_layout():
 		     "11 pm"]:
 		liTime.insert(END, item)
 
-		
+	fBlock.grid(row=3,column=4)
 	
 	#Deterrents
 	
+	fDeterrents= Frame(setting)
 	intDetType=IntVar()
 	
-	lbDet=Label(root,text="Deterrents")
-	lbDet.grid(row=0,column=6)
-	fDets=Frame(root)
+#<<<<<<< HEAD
+	lbDet=Label(fDeterrents,text="Deterrents")
+	lSpace=Label(fDeterrents,text="")
+	lSpace.grid(row=0,column=0)
+	lbDet.grid(row=1,column=6)
+	fDets=Frame(fDeterrents)
+#=======
+#	lbDet=Label(root,text="Deterrents")
+#	lbDet.grid(row=0,column=6)
+#	fDets=Frame(root)
+#>>>>>>> 912814b6d4c9c44d2fffb8f7b4d3e953a44b4b29
 	rbDetRadios=[]
 	rbDetRadios.append(Radiobutton(fDets,text="Only Block", variable=intDetType,value=DET_TYPE_DENY))
 	rbDetRadios.append(Radiobutton(fDets,text="Type Deterrent", variable=intDetType,value=DET_TYPE_TYPE))
@@ -500,13 +554,111 @@ def build_layout():
 	bEditWindow.config(command=roleWindowEDIT)
 	bEditWindow.grid(row=0,column=1)
 	roleFrame.grid(row=4,column=0)
+#<<<<<<< HEAD
+	
+	fDets.grid(row=3,column=6)
+	fDeterrents.grid(row=3,column=6)
+	
+	
+#=======
 
-	fDets.grid(row=2,column=6)	
+#	fDets.grid(row=2,column=6)	
+#>>>>>>> 912814b6d4c9c44d2fffb8f7b4d3e953a44b4b29
 	roleListLoad()
+	
+	fProjInfo = Frame(setting)
+	lProjName= Label(fProjInfo, text="Project Name")
+	eProjName=Entry(fProjInfo)
+	bProjCommit = Button(fProjInfo,text="Commit Changes",command=saveFile)
+	lProjName.grid(row=0,column=0)
+	eProjName.grid(row=0,column=1)
+	bProjCommit.grid(row=0,column=2,)
+	fProjInfo.grid(row=0,column=4)
+	
+	
+	if iProjType == 1:
+		openProject()
+	elif iProjType ==2:
+		pass
+	else:
+		assert ( 0) #should not come here
 
-def main():
+def dEditProj():
+	global iProjType
+	iProjType=1
 	build_layout()
+	
+def dNewProj():
+	global iProjType
+	iProjType=2
+	build_layout()
+	
+def dDeleteProj():
+	pass
+
+	
+def projectWindow():
+	global projects, lbProjects, iProjType
+	projects= Tk()
+	
+	
+	
+	lProjects=Label(projects, text="Projects")
+	lProjects.grid(row=0,column=0,sticky=W, )
+	lbProjects = Listbox(projects)
+	lbProjects.grid(row=1,column=0, pady=3, padx=3)
+	frame=Frame(projects)
+	bProjectsE=Button(frame,text="Edit",command=dEditProj)
+	bProjectsN=Button(frame,text="New",command=dNewProj)
+	bProjectsD=Button(frame,text="Delete",command=dDeleteProj)
+	bProjectsE.grid(row=0,column=0)
+	bProjectsN.grid(row=0,column=1)
+	bProjectsD.grid(row=0,column=2)
+	frame.grid(row=5,column=0)
+	
+	getProjects()
+	
 	mainloop()
+	
+def openProject():
+	global lbProjects, projectsList, lbSiteList, mySites
+	pjname=projectsList[int(lbProjects.curselection()[0])]
+	infile = open(PROJECT_DIR+pjname)
+	if infile != None:
+		mySites = pickle.load(infile)
+		print "filename: ", pjname
+		print "mysites: ", mySites
+		if mySites == None or len(mySites) == 0:
+			return
+		if lbSiteList == None:
+			lbSiteList=Listbox()
+		lbSiteList.delete(0, END)
+		for k in mySites:
+			lbSiteList.insert(END, k['url'])
+		
+
+	
+def getProjects():
+	global lbProjects, projectsList, projects
+	
+	
+	projectsList=[]
+	lbProjects.delete(0,END)
+	pro=os.listdir(PROJECT_DIR)
+	
+	for i in pro:
+		file=open(PROJECT_DIR+i)
+		projectsList.append(i)
+		lbProjects.insert(END,i)
+	
+	
+	#build_layout()
+	#mainloop()
+	
+def main():
+	#build_layout()
+	projectWindow()
+	
 
 if __name__ == "__main__":
 	main()
