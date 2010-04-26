e4b73f53034ffaebbfc5c101830d486a76eec78b
Adam Comella <rigdern@gmail.com>
2010-04-26 18:49:28 -0400

    Preparation for integration of web server with filter.
    Web server code has been cleaned up. Some code has been added to extract POST data and other request parameters.

--- filter/web_server.py
+++ filter/web_server.py
@@ -1,4 +1,4 @@
-import BaseHTTPServer
+import BaseHTTPServer, cgi
 from filter import Filter
 
 # Changes to the hosts file don't seem to be recoginzed right away. Maybe we
@@ -6,46 +6,65 @@ from filter import Filter
 # Twisted looks promising.
 # Make multithreaded so it can handle multiple connections simultaneously.
 
+def html_dict(d):
+  ret = ''
+  for k, v in d.iteritems():
+    ret += '<b>%s:</b> [%s]<br>\n'%(k, v)
+  return ret
+
+class Request(object):
+  def __init__(self):
+    pass
+
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_GET(self):
-    host = self.headers['Host']
-    
-    print 'request'
-    if host == '127.0.0.1':
-      print 'unblock'
-      self.handle_unblock_request()
-    else:
-      print 'deter'
-      self.handle_deterrent()
-
-  def handle_unblock_request(self):
     global flter
     
     self.send_response(200)
     self.send_header("Content-type", "text/html")
     self.end_headers()
     
-    url = self.path[1:]
-    # XXX This should only be possible for certain URL's. In fact, it might be
-    # appropriate to disabe this. The server itself will handle the unblocking
-    # when appropriate.
-    flter.unblock(url)
-    print 'Unblocked [%s]'%url
+    host = self.headers['Host']
+    req = {}
+    req['Path'] = self.path
+    ret = ''
+    ret += html_dict(req)
+    if self.command == 'POST':
+      form = cgi.FieldStorage(fp=self.rfile,
+        headers=self.headers,
+        environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type']})
+      post = {}
+      for field in form.keys():
+        post[field] = form[field].value
+      ret += '<br><br>POST data:<br>\n'
+      ret += html_dict(post)
+    ret += """<form method="post" action="%s">
+    Name: <input type="text" name="name"><br>
+    <input type="submit" name="submit" value="Submit">
+    </form>"""%self.path
     
-    self.wfile.write('unblocked [%s]'%url)
+    self.wfile.write(ret)
     self.wfile.close()
-  
-  def handle_deterrent(self):
-    self.send_response(200)
-    self.send_header("Content-type", "text/html")
-    self.end_headers()
+    return
     
-    self.wfile.write('show deterrent')
+    print 'request'
+    if host == '127.0.0.1':
+      print 'unblock'
+      ret = flter.undeter_requested()
+      if ret == True:
+        self.wfile.write("refresh the page")
+      else:
+        self.wfile.write(ret)
+    else:
+      print 'deter'
+      self.wfile.write(flter.website_requested(host))
+    
     self.wfile.close()
 
   do_POST = do_GET
 
 flter = Filter('configs/sampler')
+flter.start()
 addr = ('127.0.0.1', 80)
 httpd = BaseHTTPServer.HTTPServer(addr, RequestHandler)
 try:
