2608f63a907dda2c95ed817d44825807a71df736
Adam Comella <rigdern@gmail.com>
2010-04-25 23:31:49 -0400

    Integrated web server and filter.
    The web server code has been cleaned up. Some of its code (for example, the hosts file management code) has been moved to the filter. The filter rules are unfortunately only appropriate if we were doing the proxying technique. The code might not work as expected when run.

--- filter/filter.py
+++ filter/filter.py
@@ -79,8 +79,53 @@ class RuleFactory:
     klass = RuleFactory.method_rule_map[params['Method']]
     return klass(params)
 
+# XXX Path to hosts file should depend on the operating system
+class HostsFile(object):
+  UNIX_PATH = '/etc/hosts'
+  def __init__(self):
+    self.path = HostsFile.UNIX_PATH
+    self.orig_hosts_data = open(self.path).read()
+    self.hosts = self._read_hosts(self.path)
+  
+  def add(self, host_name, addr):
+    addrs = self.hosts.setdefault(host_name, set())
+    addrs.add(addr)
+  
+  def remove(self, host_name):
+    if host_name in self.hosts:
+      del self.hosts[host_name]
+      return True
+    return False
+  
+  def save(self):
+    self._write_hosts(self.hosts, self.path)
+  
+  def restore(self):
+    fp = open('/etc/hosts', 'w')
+    fp.write(self.orig_hosts_data)
+    fp.close()
+  
+  def _read_hosts(self, path):
+    hosts = {}
+    for line in open(path).readlines():
+      if '#' in line:
+        line = line[:line.index('#')]
+      parts = line.split()
+      if len(parts) == 2:
+        addrs = hosts.setdefault(parts[1], set())
+        addrs.add(parts[0])
+    return hosts
+  
+  def _write_hosts(self, hosts, path):
+    fp = open(path, 'w')
+    for name, addrs in hosts.iteritems():
+      for addr in addrs:
+        fp.write('%s\t%s\n'%(addr, name))
+    fp.close()
+
 class Filter(object):
   def __init__(self, config_path):
+    self.hosts = HostsFile()
     self.load_role_models(ROLE_FILE_NAME)
     self.load_websites(config_path)
   
@@ -101,7 +146,10 @@ class Filter(object):
       else:
         deterrent = Deterrent(deterrent_type)
       self.websites[address] = Website(address, rule, deterrent)
-    
+  
+  def shut_down(self):
+    self.hosts.restore()
+  
   def website_visited(self, url):
     website = self.websites[url]
     website.rule.website_visited()
@@ -116,32 +164,37 @@ class Filter(object):
     return website.blocked
   
   def _block(self, website):
+    self.hosts.add(website.address, '127.0.0.1')
+    self.hosts.save()
     website.blocked = True
   
-  def _unblock(self, url):
+  def _unblock(self, website):
+    self.hosts.remove(website.address)
+    self.hosts.save()
     website.blocked = False
 
-f = Filter("configs/sampler")
-dets = ['Deny', 'Type a Long String', 'Role Model']
-for url, w in f.websites.iteritems():
-  print url, w.rule.__class__.__name__
-  if w.rule.__class__ == AlwaysBlockRule:
-    print '\tAlways block'
-  elif w.rule.__class__ == TimeToleranceRule:
-    print '\tUse time:', w.rule.allowed_duration
-    print '\tBreak time:', w.rule.block_duration
-  elif w.rule.__class__ == BlockSchedulingRule:
-    print '\tAllowed hours:', w.rule.allowed_hours
-  else:
-    print '\tUnknown rule:', w.rule.__class__
-  print '\tDeterrent:', dets[w.deterrent.type],
-  if w.deterrent.type == DET_TYPE_ROLES:
-    print '(%s)'%w.deterrent.role_model.name
-  else:
-    print
+if __name__ == '__main__':
+  f = Filter("configs/sampler")
+  dets = ['Deny', 'Type a Long String', 'Role Model']
+  for url, w in f.websites.iteritems():
+    print url, w.rule.__class__.__name__
+    if w.rule.__class__ == AlwaysBlockRule:
+      print '\tAlways block'
+    elif w.rule.__class__ == TimeToleranceRule:
+      print '\tUse time:', w.rule.allowed_duration
+      print '\tBreak time:', w.rule.block_duration
+    elif w.rule.__class__ == BlockSchedulingRule:
+      print '\tAllowed hours:', w.rule.allowed_hours
+    else:
+      print '\tUnknown rule:', w.rule.__class__
+    print '\tDeterrent:', dets[w.deterrent.type],
+    if w.deterrent.type == DET_TYPE_ROLES:
+      print '(%s)'%w.deterrent.role_model.name
+    else:
+      print
 
-print '-'*40
-print 'ROLE MODELS'
-for rm in f.role_models.itervalues():
-  print '%s (%s)'%(rm.name, rm.picture_path)
-  print '\t' + '\n\t'.join(rm.quotes)
+  print '-'*40
+  print 'ROLE MODELS'
+  for rm in f.role_models.itervalues():
+    print '%s (%s)'%(rm.name, rm.picture_path)
+    print '\t' + '\n\t'.join(rm.quotes)
