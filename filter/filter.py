3c00e0a68941bad8e0c4c522bc78069ee20a6684
Adam Comella <rigdern@gmail.com>
2010-04-29 22:34:43 -0400

    Added comments to filter code.

--- filter/filter.py
+++ filter/filter.py
@@ -23,6 +23,8 @@ def find_if(pred, seq):
 
 
 class RoleModel(object):
+    """Represents a role model to be displayed by the filter. Includes a name,
+    picture, and quotes."""
     def __init__(self, params):
         self.name = params['Name']
         self.quotes = params['QuotesList']
@@ -30,6 +32,7 @@ class RoleModel(object):
 
 
 class AbstractRule(object):
+    """Describes the interface that must be implemented by filter rules."""
     def __init__(self, flter, address, deterrent, params):
         self.filter = flter
         self.address = address
@@ -116,6 +119,7 @@ class BlockSchedulingRule(AbstractRule):
         self._establish_timer(self._next_deterred_hour(), self._hook_address)
 
     def _establish_timer(self, end_time, callback):
+        """Create and start a timer to call *callback* at *end_time*."""
         # XXX What if now is more in the future than next_time by the time we get
         # to the subtraction code? Race condition.
         if end_time is not None:
@@ -127,9 +131,13 @@ class BlockSchedulingRule(AbstractRule):
             self.timer = None
 
     def _current_hour(self):
+        """Return the current hour as an integer between 0 and 23"""
         return datetime.datetime.now().hour
 
     def _next_hour(self, allowed):
+        """If *allowed* is true, return the next allowed hour. If *allowed* is
+        false, return the next deterred hour. Return None if there is no such
+        next allowed/blocked hour."""
         # If we're looking for an allowed hour, make sure at least one exists.
         # If we're looking for a deterred hour, make sure at least one exists.
         if allowed and len(self.allowed_hours) == 0 \
@@ -152,13 +160,16 @@ class BlockSchedulingRule(AbstractRule):
         return time.mktime(base.replace(hour=hour).timetuple())
 
     def _next_allowed_hour(self):
+        """Return the next allowed hour"""
         return self._next_hour(True)
 
     def _next_deterred_hour(self):
+        """Return the next deterred hour"""
         return self._next_hour(False)
 
 
 class RuleFactory:
+    """Returns an instance of the appropriate Rule class given a rule type."""
     method_rule_map = [DeterOnceRule, TimeToleranceRule, BlockSchedulingRule]
 
     @staticmethod
@@ -169,6 +180,10 @@ class RuleFactory:
 
 # XXX Path to hosts file for Windows shouldn't assume it'll be no the C drive.
 class HostsFile(object):
+    """An abstraction to represent the hosts file. Functions such as *add* and
+    *remove* change the in-memory object. No changes are made to disk until
+    *save* is called. The hosts file can be returned to its original state
+    by calling *restore*."""
     UNIX_PATH = '/etc/hosts'
     WINDOWS_PATH = 'C:\\Windows\\System32\\Drivers\\etc\\hosts'
 
@@ -182,10 +197,12 @@ class HostsFile(object):
         self.hosts = self._read_hosts(self.path)
 
     def add(self, host_name, addr):
+        """Add an entry to associate *host_name* with *addr*."""
         addrs = self.hosts.setdefault(host_name, set())
         addrs.add(addr)
 
     def remove(self, host_name):
+        """Remove all entries involving *host_name*."""
         if host_name in self.hosts:
             del self.hosts[host_name]
             return True
@@ -196,6 +213,8 @@ class HostsFile(object):
         os.system("ipconfig /flushdns")
 
     def restore(self):
+        """Return the hosts file to the state that it was in when this HostsFile
+        object was created."""
         fp = open(self.path, 'w')
         fp.write(self.orig_hosts_data)
         fp.close()
@@ -219,24 +238,33 @@ class HostsFile(object):
         fp.close()
 
     def _back_up_hosts_file(self, path):
+        """Create a back up of the hosts file called hosts-bkup in the same
+        directory as the hosts file."""
         bkup_path = os.path.join(os.path.dirname(self.path), 'hosts-bkup')
         if not os.path.exists(bkup_path):
             shutil.copy(path, bkup_path)
 
 
 class Filter(object):
+    """Represents a web filter. Provides an interface for configuring which
+    websites should be filtered and by what rules and deterrents the filtering
+    should happen. The filter is expected to be initialized from a project
+    configuration file created by the Configuration Utility."""
     def __init__(self, config_path):
         self.hosts = HostsFile()
         self.load_role_models(ROLE_FILE_NAME)
         self.load_rules(config_path)
 
     def load_role_models(self, path):
+        """Load the role models file into memory."""
         self.role_models = {}
         if os.path.exists(path):
             for model in pickle.load(open(path)):
                 self.role_models[model['Name']] = RoleModel(model)
 
     def load_rules(self, config_path):
+        """Read the rules and deterrents into memory from the project
+        configuration file stored at *config_path*."""
         self.rules = {}
         for raw_rule in pickle.load(open(config_path)):
             address = raw_rule['url']
@@ -249,12 +277,19 @@ class Filter(object):
             self.rules[address] = RuleFactory.rule_for_dict(self, address, deterrent, raw_rule['BlockConfig'])
 
     def website_requested(self, request):
+        """Returns the HTML that should be rendered to the user when *request*
+        has been made by the user."""
         try:
             return self._html_wrap(self.rules[request.target_host].deterrent.render(request))
         except KeyError:
             return "Host not supposed to be used with filter: %s" % request.target_host
 
     def undeter_requested(self, request):
+        """Either returns True or some HTML. If True is returned, it means
+        that the website associated with *request* has been unblocked and
+        that the user should be redirected to the page they requested. If HTML
+        is returned, this means that the website is still blocked and the HTML
+        should be rendered to the user instead."""
         try:
             address = request.target_host
             rule = self.rules[address]
@@ -269,21 +304,28 @@ class Filter(object):
             return "Host not supposed to be used with filter: %s" % address
 
     def start(self):
+        """The filter should now be in effect. Let the rules begin!"""
         [rule.enable() for rule in self.rules.itervalues()]
 
     def shut_down(self):
+        """Stop filtering. Inform the rules that they are no longer needed and
+        restore the hosts file to its original form."""
         [rule.disable() for rule in self.rules.itervalues()]
         self.hosts.restore()
 
     def hook(self, address):
+        """Begin intercepting requests for *address*."""
         self.hosts.add(address, '127.0.0.1')
         self.hosts.save()
 
     def unhook(self, address):
+        """Stop intercepting requests for *address*."""
         self.hosts.remove(address)
         self.hosts.save()
 
     def _html_wrap(self, body):
+        """The HTML layout that our filter returns to the user. Returns the
+        layout with *body* interpolated into the middle of it."""
         title = "Pineapple Web Filter"
         blocked = "BLOCKED!"
         content = """
--- filter/web_server.py
+++ filter/web_server.py
@@ -14,6 +14,12 @@ WEBROOT = "webroot"
 
 
 class Request(object):
+    """Represents a webrequest made by the user. Includes information that
+    the filter needs to know about the request.
+    *webroot* - The location of all static files our web server can serve.
+    *path* - The path portion of the URL that the user requested.
+    *post* - A dictionary representing the form data the user has submitted.
+    *target_host* - The hostname that the user is trying to reach."""
     def __init__(self, webroot=None, path=None, post_data=None,
         target_host=None):
         self.webroot = webroot
@@ -23,7 +29,13 @@ class Request(object):
 
 
 class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    """The core of the web server. An instance is created for each web
+    request."""
     def do_HEAD(self):
+        """Handle an HTTP HEAD request. SimpleHTTPRequestHandler only serves
+        files from the current directory. We want to serve files only from
+        WEBROOT so we make WEBROOT the current directory and then let
+        SimpleHTTPRequestHandler do its job."""
         os.chdir(WEBROOT)
         try:
             SimpleHTTPServer.SimpleHTTPRequestHandler.do_HEAD(self)
@@ -31,6 +43,17 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             os.chdir('..')
 
     def handle_request(self):
+        """Handles the majority of web requests from the user. It has a few
+        general modes. The mode depends on the hostname and the request type:
+        1. 127.0.0.1, GET
+           Serves static files from WEBROOT. Used for serving the "captcha"
+           images and the role model pictures.
+        2. 127.0.0.1, POST
+           The user wants undeter the website stored in the *host* POST
+           parameter. See if they've cleared the deterrent.
+        3. not 127.0.0.1, GET or POST
+           The user is trying to access a blocked webpage. Show them the
+           appropriate deterrent."""
         global flter
 
         host = self.headers['Host']
@@ -38,13 +61,14 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
         if host == '127.0.0.1':
             if self.command == 'GET' and self.path != '/':
-                # Serve a static file.
+                # Mode 1: Serve a static file.
                 os.chdir('webroot')
                 try:
                     SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
                 finally:
                     os.chdir('..')
             else:
+                # Mode 2: See if the user has properly passed the deterrent.
                 try:
                     request.target_host = request.post['host']
                     ret = flter.undeter_requested(request)
@@ -57,10 +81,14 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                 else:
                     self.send_ok(ret)
         else:
+            # Mode 3: The user is trying to access a blocked page. Deter them.
             request.target_host = host
             self.send_ok(flter.website_requested(request))
 
     def post_data(self):
+        """Returns the raw form data as a python dictionary. The keys are the
+        form element names and the values are the user-submitted form
+        values."""
         post_data = {}
         if self.command == 'POST' and 'Content-Type' in self.headers:
             form = cgi.FieldStorage(fp=self.rfile,
@@ -71,6 +99,7 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         return post_data
 
     def send_ok(self, content):
+        """Returns *content* to the browser."""
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -78,14 +107,19 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.wfile.close()
 
     def send_redirect(self, redirect_url):
+        """Redirects the browser to *redirect_url*."""
         self.send_response(301)
         self.send_header("Location", redirect_url)
         self.end_headers()
-
+    
+    # Set GET and POST requests to be handled by the handle_request function.
     do_POST = do_GET = handle_request
 
 
 def main():
+    """Get everything started. Initialize the filter from the configuration
+    file specified by the first command line argument. Start the web server
+    for the filter."""
     global flter
 
     if len(sys.argv) != 2:
@@ -94,6 +128,8 @@ def main():
 
     flter = Filter(sys.argv[1])
     flter.start()
+    # If the WEBROOT directory doesn't exist, create it and make sure
+    # the user can write to it.
     if not os.path.exists(WEBROOT):
         old = os.umask(0002)
         try:
@@ -110,6 +146,7 @@ def main():
     finally:
         flter.shut_down()
         for file_name in os.listdir(WEBROOT):
+            # Delete the "captchas."
             if file_name.endswith(".JPEG"):
                 os.remove(os.path.join(WEBROOT, file_name))
     print 'done'
